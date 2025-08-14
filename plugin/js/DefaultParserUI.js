crybx/WebToEpub

Rename ChaptersUI back to ChapterUrlsUI

plugin/js/DefaultParserUI.js
@@ -137,10 +137,10 @@ class DefaultParserUI {
 
     static setDefaultParserUiVisibility(isVisible) {
         // toggle mode
-        ChaptersUI.setVisibileUI(!isVisible);
+        ChapterUrlsUI.setVisibileUI(!isVisible);
         if (isVisible) {
-            ChaptersUI.getEditChaptersUrlsInput().hidden = true;
-            ChaptersUI.modifyApplyChangesButtons(button => button.hidden = true);
+            ChapterUrlsUI.getEditChaptersUrlsInput().hidden = true;
+            ChapterUrlsUI.modifyApplyChangesButtons(button => button.hidden = true);
             document.getElementById("editURLsHint").hidden = true;
         }
         document.getElementById("defaultParserSection").hidden = !isVisible;

crybx/WebToEpub-Codex

plugin/js/DefaultParserUI.js
@@ -136,10 +136,10 @@ class DefaultParserUI { // eslint-disable-line no-unused-vars
 
     static setDefaultParserUiVisibility(isVisible) {
         // toggle mode
-        ChaptersUI.setVisibleUI(!isVisible);
+        ChapterUrlsUI.setVisibleUI(!isVisible);
         if (isVisible) {
-            ChaptersUI.getEditChaptersUrlsInput().hidden = true;
-            ChaptersUI.modifyApplyChangesButtons(button => button.hidden = true);
+            ChapterUrlsUI.getEditChaptersUrlsInput().hidden = true;
+            ChapterUrlsUI.modifyApplyChangesButtons(button => button.hidden = true);
             document.getElementById("editURLsHint").hidden = true;
         }
         document.getElementById("defaultParserSection").hidden = !isVisible;